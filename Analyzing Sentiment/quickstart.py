11751ceec718a9a01ae14bb280a5c5fc7639a7d3
ByungSoo Ko <kobiso62@gmail.com>
2017-05-14 17:07:19 +0900

    Extract emotions from csv file

--- Analyzing Sentiment/quickstart.py
+++ Analyzing Sentiment/quickstart.py
@@ -13,7 +13,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+import csv
 
 def run_quickstart():
     # [START language_quickstart]
@@ -23,17 +23,40 @@ def run_quickstart():
     # Instantiates a client
     language_client = language.Client()
 
-    # The text to analyze
-    text = 'Hello, world!'
-    document = language_client.document_from_text(text)
+    f = open('data/facebook_statuses.csv','r')
+    csvReader = csv.reader(f)
+    new_rows_list = []
 
-    # Detects the sentiment of the text
-    sentiment = document.analyze_sentiment().sentiment
+    for row in csvReader:
 
-    print('Text: {}'.format(text))
-    print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
-    # [END language_quickstart]
+        # print('Text: {}'.format(text))
+        # print('Sentiment: {}, {}'.format(sentiment.score, sentiment.magnitude))
 
+        if row[0]=='comment: ':
+            text = row[1]
+            try:
+                document = language_client.document_from_text(text)
+                # Detects the sentiment of the text
+                sentiment = document.analyze_sentiment().sentiment
+                new_row = [row[0], row[1], sentiment.score, sentiment.magnitude]
+            except Exception:
+                print ('error')
+            # print (text)
+            # print (new_row)
+        else:
+            try:
+                new_row = [row[0], row[1], row[2], row[3], row[4], row[5], row[6], row[7], row[8], row[9], row[10], row[11], row[12], row[13], row[14]]
+            except Exception:
+                pass
+        new_rows_list.append(new_row)
+    f.close()
+
+    f2 = open('data/result.csv','w', newline='')
+    writer = csv.writer(f2)
+    writer.writerows(new_rows_list)
+    # output = csv.DictWriter(f2, delimiter=',', lineterminator='\n')
+    # output.writerows(new_rows_list)
+    f2.close()
 
 if __name__ == '__main__':
     run_quickstart()
